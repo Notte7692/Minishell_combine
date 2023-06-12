f7349047f231a3f830158d17ceddc02f132bc51e
Notte7692 <nassal92@hotmail.com>
2023-06-12 19:53:22 +0200

    push

--- srcs/commandes.c
+++ srcs/commandes.c
@@ -6,7 +6,7 @@
 /*   By: nassm <nassm@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 15:24:32 by nbechon           #+#    #+#             */
-/*   Updated: 2023/06/09 16:29:33 by nassm            ###   ########.fr       */
+/*   Updated: 2023/06/12 19:48:39 by nassm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,24 @@
 
 int	commande_echo(char **word)
 {
-	int	i;
-
-	i = 1;
-	if (word[0] == NULL)
+	bool	new_line;
+	
+	new_line = true;
+	if (ft_strncmp(*word, "echo", ft_strlen(*word)) != 0)
 		return (EXIT_FAILURE);
-	if (word[2] == NULL)
-		ft_printf ("%s\n", " ");
-	else
+	word++;
+	if (*word && ft_strncmp(*word, "-n", ft_strlen("-n") + 1) == 0)
 	{
-		if (ft_strcmp(word[1], "-n") == 0)
-			i = 2;
-		while (word[i])
-		{
-			ft_printf("%s", word[i]);
-			if (word[i + 1])
-				write(1, " ", 1);
-			i++;
-		}
-		if (ft_strcmp(word[1], "-n") != 0)
-			write(1, "\n", 1);
+		new_line = false;
+		word++;
 	}
+	while (*word)
+	{
+		write(1, *word, ft_strlen(*word));
+		word++;
+	}
+	if (new_line)
+		write(1, "\n", 1);
 	return (EXIT_SUCCESS);
 }
 
