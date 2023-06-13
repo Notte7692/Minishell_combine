b797335b568a624a30f9387570998087c70529bc
Notte7692 <nassal92@hotmail.com>
2023-06-13 16:37:29 +0200

    push

--- srcs/commandes2.c
+++ srcs/commandes2.c
@@ -6,7 +6,7 @@
 /*   By: nassm <nassm@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 15:14:28 by nbechon           #+#    #+#             */
-/*   Updated: 2023/06/13 15:37:22 by nassm            ###   ########.fr       */
+/*   Updated: 2023/06/13 16:36:13 by nassm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -165,7 +165,9 @@ int	commande_exit(char **tab)
 {
 	long    exit_code;
     int        i;
+    int        j;
 
+    j = 0;
     i = 0;
     if (ft_strncmp(tab[i], "exit", ft_strlen(tab[i])) != 0)
         return (EXIT_FAILURE);
@@ -174,16 +176,24 @@ int	commande_exit(char **tab)
         exit_code = ft_atol(tab[i]);
     else
         exit_code = get_err_code();
-    if (ft_strlen(tab[i]) > 19)
+    if (!valid_exit(tab + i) && ft_strlen(tab[i]) > 19)
     {
+        if (tab[2] != NULL)
+        {
+            while (tab[2][j])
+            {
+                if (tab[2][j] < '0' && tab[2][j] > '9')
+                {
+                    ft_fprintf(STDERR_FILENO, "exit\n");
+                    ft_fprintf(STDERR_FILENO, "exit: too many arguments\n");
+                    return (EXIT_FAILURE);
+                }
+                j++;
+            }
+        }
         ft_fprintf(STDERR_FILENO, "exit\n");
-        ft_fprintf(STDERR_FILENO, "exit: %s: numeric argument required\n", tab[1]);
-        exit_code = 255;
-    }
-    else if (!valid_exit(tab + i))
-    {
-        ft_fprintf(STDERR_FILENO, "exit\n");
-        ft_fprintf(STDERR_FILENO, "exit: numeric argument required\n");
+        ft_fprintf(STDERR_FILENO, "exit: %s: numeric argument required\n",
+            tab[1]);
         exit_code = 255;
     }
     else if (tab[i] && tab[++i])
